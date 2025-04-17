b1039f4ce3f89ccf5d7a4b22ba4f8da39045df36
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2025-04-17 10:47:33 +0300

    fix: fix create bookmark schema and add bullmq package for queue processing

--- src/common/validation/schemas/bookmark/bookmark.schema.ts
+++ src/common/validation/schemas/bookmark/bookmark.schema.ts
@@ -7,9 +7,10 @@ export const createBookmarkSchema = createInsertSchema(bookmarks, {
     .string({ required_error: 'URL is required' })
     .url('Please enter a valid URL'),
   title: z
-    .string({ required_error: 'Title is required' })
-    .min(1, 'Title cannot be empty')
-    .max(255, 'Title must be under 255 characters'),
+    .string()
+    .max(255, 'Title must be under 255 characters')
+    .nullable()
+    .optional(),
   description: z.string().max(10_000, 'Description is too long').optional(),
   thumbnail: z
     .string()

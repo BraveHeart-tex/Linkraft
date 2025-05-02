9d9640217b57c7dc4f533de3c13d2142bbd6a1f8
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2025-05-02 14:30:25 +0000

    feat: enhance full-text search functionality with schema changes - Modified schema.ts to include tsvector columns for full-text search in tags and bookmarks. - Added GIN indexes for improved search performance on tsvector columns.

--- src/db/drizzle.utils.ts
+++ src/db/drizzle.utils.ts
@@ -1,6 +1,14 @@
 import { SQL, sql } from 'drizzle-orm';
-import { AnyPgColumn } from 'drizzle-orm/pg-core';
+import { AnyPgColumn, customType } from 'drizzle-orm/pg-core';
 
 export const lower = (email: AnyPgColumn): SQL => {
   return sql`lower(${email})`;
 };
+
+export const tsvector = customType<{
+  data: string;
+}>({
+  dataType() {
+    return `tsvector`;
+  },
+});
